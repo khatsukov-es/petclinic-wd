bfe99866b3a69b72f3f7a7e9b17e407bb8e99f8b
Иван Бонькин <i.bonkin@pflb.ru>
2017-08-29 15:59:48 +0300

    added simple test

--- src/test/java/ru/pflb/wd/PetclinicTest.java
+++ src/test/java/ru/pflb/wd/PetclinicTest.java
@@ -1,8 +1,13 @@
 package ru.pflb.wd;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author <a href="mailto:8445322@gmail.com">Ivan Bonkin</a>.
@@ -10,10 +15,21 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class PetclinicTest {
 
     @Test
-    public void generateKingMoves() {
-        WebDriver firefoxDriver = new FirefoxDriver();
+    public void shouldDisplayPetTypes() {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        firefoxDriver.get("http://localhost:4200/petclinic/");
+        driver.get("http://localhost:4200/petclinic/");
+
+        driver.findElement(By.linkText("Pet Types")).click();
+
+        List<WebElement> h2 = driver.findElements(By.tagName("h2"));
+        for (WebElement we : h2) {
+            if ("Not Found - 404 error".equals(we.getText())) {
+                System.out.println("");
+            }
+        }
+
         System.out.println("");
     }
 }
