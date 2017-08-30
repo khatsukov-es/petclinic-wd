ee89e20477b5961124c3341a7d48a2021942a71e
Иван Бонькин <i.bonkin@pflb.ru>
2017-08-30 18:10:29 +0300

    add new pet test

--- src/test/java/ru/pflb/wd/PetclinicTest.java
+++ src/test/java/ru/pflb/wd/PetclinicTest.java
@@ -1,5 +1,8 @@
 package ru.pflb.wd;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -9,15 +12,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.openqa.selenium.firefox.FirefoxDriver.PROFILE;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.openqa.selenium.firefox.FirefoxDriver.PROFILE;
 
 /**
  * @author <a href="mailto:8445322@gmail.com">Ivan Bonkin</a>.
@@ -41,8 +45,10 @@ public class PetclinicTest {
         driver.findElement(By.xpath("//h2[text()='Not Found - 404 error']"));
     }
 
-    @Test
-    public void shouldFindOwnerAndChangeHisName() throws IOException, URISyntaxException {
+    private WebDriver driver;
+
+    @Before
+    public void initDriver() throws URISyntaxException, IOException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         FirefoxProfile profile = new FirefoxProfile();
         File firebug = new File(FirefoxDriver.class.getResource("/firebug-1.12.7-fx.xpi").toURI());
@@ -51,9 +57,18 @@ public class PetclinicTest {
         profile.addExtension(firepath);
         profile.setPreference("extensions.firebug.showFirstRunPage", false);
         capabilities.setCapability(PROFILE, profile);
-        WebDriver driver = new FirefoxDriver(capabilities);
+        driver = new FirefoxDriver(capabilities);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, SECONDS);
+    }
+
+    @After
+    public void closeDriver() {
+        driver.quit();
+    }
+
+    @Test
+    public void shouldFindOwnerAndChangeHisName() {
 
         // открываем PetClinic по ссылке
         driver.get("http://localhost:8080/");
@@ -91,6 +106,48 @@ public class PetclinicTest {
     }
 
 
+    @Test
+    public void shouldAddNewPet() {
+        // открываем PetClinic по ссылке
+        driver.get("http://localhost:8080/");
+
+        // клик по меню Find Owners
+        driver.findElement(By.xpath("//span[text()='Find owners']")).click();
+
+        // ввод фамилии Franklin в поле Last name
+        driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("Franklin");
+
+        // клик по Find Owner
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        String rndVal = RandomStringUtils.randomAlphabetic(4);
+
+        for (String animal : Arrays.asList("bird", "cat", "dog", "hamster", "lizard", "snake")) {
+            // клик по Add new Pet
+            driver.findElement(By.xpath("//a[starts-with(text(), 'Add') and contains(text(), 'New Pet')]")).click();
+
+            // в выпадающем списке вид тип животного
+            Select select = new Select(driver.findElement(By.xpath("//select[@id='type']")));
+            select.selectByValue(animal);
+            // в поле имя ввести уникальное имя по виду животного в формате типрандом (слитно)
+            driver.findElement(By.xpath("//input[@id='name']")).sendKeys(animal + rndVal);
+
+            // в поле даты рождения ввести 2017/01/01
+            driver.findElement(By.xpath("//input[@id='birthDate']")).sendKeys("2017/01/01");
+
+            // кликнуть Update Pet
+            driver.findElement(By.xpath("//button[text()='Update Pet']")).click();
+
+            // должен быть найден только один блок с новым комбинированным именем и относящимся к нему виду животного
+            assertThat(driver.findElements(By.xpath(String.format(
+                            "//dl[self::*/descendant::dd/text()[1] ='%1$s%2$s' and self::*/descendant::dt[.='Type']/following-sibling::dd/text()[1] = '%1$s']",
+                    animal, rndVal
+            ))).size())
+                    .isEqualTo(1);
+        }
+
+
+    }
 
     /**
      * Домашнее задание.
