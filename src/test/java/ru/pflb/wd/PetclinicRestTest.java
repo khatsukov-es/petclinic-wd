0b08452cc8d9f54676d0bb8ac1479a3943923bee
Иван Бонькин <i.bonkin@pflb.ru>
2017-08-31 17:00:07 +0300

    added description

--- src/test/java/ru/pflb/wd/PetclinicRestTest.java
+++ src/test/java/ru/pflb/wd/PetclinicRestTest.java
@@ -47,27 +47,41 @@ public class PetclinicRestTest {
     @Test
     public void shouldFindOwnerAndAddPet() {
 
+        // получение JSON описания хозяина с фамилией Franklin
         JSONObject user = findOwner("Franklin");
 
+        // генерация произвольного имени домашнего животного в формате Xxxxx.
         String name = capitalize(RandomStringUtils.randomAlphabetic(5).toLowerCase());
 
+        // составления тела запроса на добавление нового домашенго животного
         JSONObject petJsonObj = new JSONObject()
+                // id = null
                 .put("id", JSONObject.NULL)
+                // name = сгенерированное имя
                 .put("name", name)
+                // дату рождения выбираем равной текущий день минус 7 дней
                 .put("birthDate", LocalDate.now().minusDays(7).format(DateTimeFormatter.ofPattern("yyyy/MM/dd")))
+                // в поле тип вводим - bird
                 .put("type", new JSONObject().put("id", 5).put("name", "bird"))
+                // в поле хозяина вводим JSON описания хозяина
                 .put("owner", user);
 
         Integer petId = given()
+                // Content-type для запроса - формат body запроса
                 .contentType("application/json")
+                // тело запроса
                 .body(petJsonObj.toString())
+                // URI REST сервера
                 .baseUri(BASE_URI)
                 .when()
+                // путь относительно REST сервера
                 .post("/api/pets")
                 .then()
-                // expect 2xx response code
+                // ожилаение 2xx кодов - успешных
                 .statusCode(equalTo(201))
+                // проверка того, что в ответе сервера был создан тот же питомец
                 .body("name", equalTo(name))
+                // возвращение id созданного питомца
                 .extract().path("id");
 
         System.out.println("Добавлен питомец с id=\"" + petId + "\"");
